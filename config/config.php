3db411120ad77b071f595bd47ddf31952291a415
taron <taron@ucraft.com>
2023-09-20 20:01:28 +0400

    Update dependencies

--- config/config.php
+++ config/config.php
@@ -3,14 +3,14 @@
 declare(strict_types=1);
 
 return [
-    'topic'            => env('KAFKA_MAILER_TOPIC'),
+    'topic'            => env('MESSENGER_TOPIC'),
 
     /*
      | In order to use our Kafka-based mail server, put this piece of code in the config/mail.php file.
      |
      'kafka-mailer' => [
           'transport' => 'kafka-mailer,
-          'topic'     => env('KAFKA_MAILER_TOPIC'),
+          'topic'     => env('MESSENGER_TOPIC'),
       ],
       |
      */
